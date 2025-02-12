daec6d59f1b71be84b114f99467144c7572757d3
greg-prk <greg.prk@kakaocorp.com>
2025-02-12 13:56:15 +0900

    feat: page adding test

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,11 +5,22 @@ import { WC } from "./WC.jsx";
 import "./Home.css";
 
 export const Home = () => {
-    return (
-        <div>
-                <WC>
-                    <p className="display">Congratulations! You just created your first React Plugin.</p>
-                </WC>
-       </div>
-    );
+  const duplicateMasterSpread = () => {
+    const app = require("indesign").app;
+    const doc = app.activeDocument;
+    const masterSpread = doc.masterSpreads.itemByName("H-내지B");
+    const leftPage = doc.pages.add();
+    leftPage.appliedMaster = masterSpread;
+    const rightPage = doc.pages.add();
+    rightPage.appliedMaster = masterSpread;
+  };
+
+  return (
+    <div>
+      <WC>
+        <p className="display">Congratulations! You just created your first React Plugin.</p>
+        <button onClick={duplicateMasterSpread}>Hello</button>
+      </WC>
+    </div>
+  );
 }
--- src/panels/Demos.jsx
+++ src/panels/Demos.jsx
@@ -3,9 +3,9 @@ import React from "react";
 import { Home } from "../components/Home.jsx";
 
 export const Demos = () => {
-    return (
-        <>
-            <Home />
-        </>
-    )
-} 
+  return (
+    <>
+      <Home />
+    </>
+  )
+}
